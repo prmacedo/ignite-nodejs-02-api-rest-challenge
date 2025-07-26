a204fad1bf8edb57755453adc9d7648cc5dd1d9c
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2025-07-26 16:00:40 -0300

    feat: add user meals metrics route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,12 +2,71 @@ import { FastifyInstance } from "fastify";
 import z from "zod";
 import { randomUUID } from "node:crypto";
 import { knex } from "../database";
+import { checkUserIdExists } from "../middlewares/check-user-id-exists";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get("/", async () => {
     const users = await knex.table("users").select("*");
     return { users };
   });
+
+  app.get(
+    "/metrics",
+    { preHandler: [checkUserIdExists] },
+    async (request, reply) => {
+      const { userId } = request.cookies;
+
+      const totalMeals = await knex("meals")
+        .where("user_id", userId)
+        .orderBy(["date", { column: "time", order: "asc" }])
+        .select("is_on_diet");
+
+      const mealsGroupedByDiet = await knex("meals")
+        .where("user_id", userId)
+        .groupBy("is_on_diet")
+        .orderBy(["date", { column: "time", order: "asc" }])
+        .select("is_on_diet")
+        .count("is_on_diet", { as: "total" });
+
+      let mealsOnDiet = 0;
+      let mealsOffDiet = 0;
+
+      for (const group of mealsGroupedByDiet) {
+        if (group.is_on_diet) {
+          mealsOnDiet = Number(group.total ?? 0);
+        } else {
+          mealsOffDiet = Number(group.total ?? 0);
+        }
+      }
+
+      const { bestOnDietStreak } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentStreak++;
+          } else {
+            acc.currentStreak = 0;
+          }
+
+          if (acc.currentStreak > acc.bestOnDietStreak) {
+            acc.bestOnDietStreak = acc.currentStreak;
+          }
+
+          return acc;
+        },
+        {
+          bestOnDietStreak: 0,
+          currentStreak: 0,
+        }
+      );
+
+      return {
+        totalMeals: totalMeals.length,
+        mealsOnDiet: mealsOnDiet,
+        mealsOffDiet: mealsOffDiet,
+        bestOnDietStreak,
+      };
+    }
+  );
 
   app.post("/", async (request, reply) => {
     const createUserBodySchema = z.object({
